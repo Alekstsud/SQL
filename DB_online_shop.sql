92d68fcec727ec10bf663c2b8ff24ad38cb4ea01
Aleks_Tsud <100237478+Alekstsud@users.noreply.github.com>
2022-09-04 18:24:28 +0200

    Add files via upload

---
+++ DB_online_shop.sql
@@ -0,0 +1,322 @@
+CREATE TABLE author (
+    author_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_author VARCHAR(50)
+);
+
+INSERT INTO author (name_author)
+VALUES ('Булгаков М.А.'),
+       ('Достоевский Ф.М.'),
+       ('Есенин С.А.'),
+       ('Пастернак Б.Л.'),
+       ('Лермонтов М.Ю.');
+
+CREATE TABLE genre (
+    genre_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_genre VARCHAR(30)
+);
+
+INSERT INTO genre(name_genre)
+VALUES ('Роман'),
+       ('Поэзия'),
+       ('Приключения');
+
+CREATE TABLE book (
+    book_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(50),
+    author_id INT NOT NULL,
+    genre_id INT,
+    price DECIMAL(8, 2),
+    amount INT,
+    FOREIGN KEY (author_id)
+        REFERENCES author (author_id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (genre_id)
+        REFERENCES genre (genre_id)
+        ON DELETE SET NULL
+);
+
+INSERT INTO book (title, author_id, genre_id, price, amount)
+VALUES  ('Мастер и Маргарита', 1, 1, 670.99, 3),
+        ('Белая гвардия ', 1, 1, 540.50, 5),
+        ('Идиот', 2, 1, 460.00, 10),
+        ('Братья Карамазовы', 2, 1, 799.01, 2),
+        ('Игрок', 2, 1, 480.50, 10),
+        ('Стихотворения и поэмы', 3, 2, 650.00, 15),
+        ('Черный человек', 3, 2, 570.20, 6),
+        ('Лирика', 4, 2, 518.99, 2);
+
+CREATE TABLE city (
+    city_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_city VARCHAR(30),
+    days_delivery INT
+);
+
+INSERT INTO city(name_city, days_delivery)
+VALUES ('Москва', 5),
+       ('Санкт-Петербург', 3),
+       ('Владивосток', 12);
+
+CREATE TABLE client (
+    client_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_client VARCHAR(50),
+    city_id INT,
+    email VARCHAR(30),
+    FOREIGN KEY (city_id) REFERENCES city (city_id)
+);
+
+INSERT INTO client(name_client, city_id, email)
+VALUES ('Баранов Павел', 3, 'baranov@test'),
+       ('Абрамова Катя', 1, 'abramova@test'),
+       ('Семенонов Иван', 2, 'semenov@test'),
+       ('Яковлева Галина', 1, 'yakovleva@test');
+
+CREATE TABLE buy(
+    buy_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_description VARCHAR(100),
+    client_id INT,
+    FOREIGN KEY (client_id) REFERENCES client (client_id)
+);
+
+INSERT INTO buy (buy_description, client_id)
+VALUES ('Доставка только вечером', 1),
+       (NULL, 3),
+       ('Упаковать каждую книгу по отдельности', 2),
+       (NULL, 1);
+
+CREATE TABLE buy_book (
+    buy_book_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id INT,
+    book_id INT,
+    amount INT,
+    FOREIGN KEY (buy_id) REFERENCES buy (buy_id),
+    FOREIGN KEY (book_id) REFERENCES book (book_id)
+);
+
+INSERT INTO buy_book(buy_id, book_id, amount)
+VALUES (1, 1, 1),
+       (1, 7, 2),
+       (1, 3, 1),
+       (2, 8, 2),
+       (3, 3, 2),
+       (3, 2, 1),
+       (3, 1, 1),
+       (4, 5, 1);
+
+CREATE TABLE step (
+    step_id INT PRIMARY KEY AUTO_INCREMENT,
+    name_step VARCHAR(30)
+);
+
+INSERT INTO step(name_step)
+VALUES ('Оплата'),
+       ('Упаковка'),
+       ('Транспортировка'),
+       ('Доставка');
+
+CREATE TABLE buy_step (
+    buy_step_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id INT,
+    step_id INT,
+    date_step_beg DATE,
+    date_step_end DATE,
+    FOREIGN KEY (buy_id) REFERENCES buy (buy_id),
+    FOREIGN KEY (step_id) REFERENCES step (step_id)
+);
+
+INSERT INTO buy_step(buy_id, step_id, date_step_beg, date_step_end)
+VALUES (1, 1, '2020-02-20', '2020-02-20'),
+       (1, 2, '2020-02-20', '2020-02-21'),
+       (1, 3, '2020-02-22', '2020-03-07'),
+       (1, 4, '2020-03-08', '2020-03-08'),
+       (2, 1, '2020-02-28', '2020-02-28'),
+       (2, 2, '2020-02-29', '2020-03-01'),
+       (2, 3, '2020-03-02', NULL),
+       (2, 4, NULL, NULL),
+       (3, 1, '2020-03-05', '2020-03-05'),
+       (3, 2, '2020-03-05', '2020-03-06'),
+       (3, 3, '2020-03-06', '2020-03-10'),
+       (3, 4, '2020-03-11', NULL),
+       (4, 1, '2020-03-20', NULL),
+       (4, 2, NULL, NULL),
+       (4, 3, NULL, NULL),
+       (4, 4, NULL, NULL);
+
+--1. Вывести все заказы Баранова Павла (id заказа, какие книги, по какой цене и в каком количестве он заказал) в отсортированном по номеру заказа и названиям книг виде.
+
+SELECT buy.buy_id, book.title, book.price, buy_book.amount
+FROM 
+    client 
+    INNER JOIN buy ON client.client_id = buy.client_id
+    INNER JOIN buy_book ON buy_book.buy_id = buy.buy_id
+    INNER JOIN book ON buy_book.book_id=book.book_id
+WHERE client.client_id = 1
+ORDER BY buy_id ASC, title ASC; 
+
+--2.  Посчитать, сколько раз была заказана каждая книга, для книги вывести ее автора (нужно посчитать, в каком количестве заказов фигурирует каждая книга).  Вывести фамилию и инициалы автора, название книги, последний столбец назвать Количество. Результат отсортировать сначала  по фамилиям авторов, а потом по названиям книг.
+
+SELECT author.name_author, book.title, COUNT(buy_book.amount) AS Количество
+FROM
+    author
+    INNER JOIN book USING(author_id)
+    LEFT JOIN buy_book ON buy_book.book_id = book.book_id
+GROUP BY author.name_author, book.title
+ORDER BY author.name_author ASC, book.title ASC;
+
+--3. Вывести города, в которых живут клиенты, оформлявшие заказы в интернет-магазине. Указать количество заказов в каждый город, этот столбец назвать Количество. Информацию вывести по убыванию количества заказов, а затем в алфавитном порядке по названию городов.
+
+SELECT city.name_city, COUNT(buy.buy_id) Количество
+FROM
+    city
+    INNER JOIN client USING(city_id)
+    INNER JOIN buy USING(client_id)
+GROUP BY city.name_city
+ORDER BY Количество DESC, city.name_city ASC;
+
+--4. Вывести номера всех оплаченных заказов и даты, когда они были оплачены.
+
+SELECT buy_id, date_step_end
+FROM
+    step
+    INNER JOIN buy_step USING(step_id)
+WHERE step_id = 1 
+  AND date_step_end IS NOT NULL
+GROUP BY buy_id, date_step_end;
+
+--5. Вывести информацию о каждом заказе: его номер, кто его сформировал (фамилия пользователя) и его стоимость (сумма произведений количества заказанных книг и их цены), в отсортированном по номеру заказа виде. Последний столбец назвать Стоимость.
+
+SELECT buy_id, name_client, SUM(buy_book.amount * book.price) AS Стоимость
+FROM 
+    buy_book
+    INNER JOIN buy USING(buy_id)
+    INNER JOIN client USING(client_id)
+    INNER JOIN book USING(book_id)
+GROUP BY name_client, buy_id
+ORDER BY buy_id;
+
+--6. Вывести номера заказов (buy_id) и названия этапов, на которых они в данный момент находятся. Если заказ доставлен –  информацию о нем не выводить. Информацию отсортировать по возрастанию buy_id.
+
+SELECT buy_id, name_step
+FROM 
+    step
+    INNER JOIN buy_step USING(step_id)
+WHERE date_step_end IS NULL AND date_step_beg IS NOT NULL
+ORDER BY buy_id ASC;
+
+--7. В таблице city для каждого города указано количество дней, за которые заказ может быть доставлен в этот город (рассматривается только этап "Транспортировка"). Для тех заказов, которые прошли этап транспортировки, вывести количество дней за которое заказ реально доставлен в город. А также, если заказ доставлен с опозданием, указать количество дней задержки, в противном случае вывести 0. В результат включить номер заказа (buy_id), а также вычисляемые столбцы Количество_дней и Опоздание. Информацию вывести в отсортированном по номеру заказа виде.
+
+SELECT buy_id, DATEDIFF(date_step_end, date_step_beg) AS Количество_дней, 
+    IF(DATEDIFF(date_step_end, date_step_beg) <= days_delivery, 0, DATEDIFF(date_step_end, date_step_beg) - days_delivery) AS Опоздание 
+FROM 
+    city
+    INNER JOIN client USING(city_id)
+    INNER JOIN buy USING(client_id)
+    INNER JOIN buy_step USING(buy_id)
+    INNER JOIN step USING(step_id)
+WHERE step_id = 3 AND date_step_end IS NOT NULL
+ORDER BY buy_id;
+
+--8. Выбрать всех клиентов, которые заказывали книги Достоевского, информацию вывести в отсортированном по алфавиту виде. В решении используйте фамилию автора, а не его id.
+
+SELECT DISTINCT(name_client)
+FROM 
+    client
+    INNER JOIN buy USING(client_id)
+    INNER JOIN buy_book USING(buy_id)
+    INNER JOIN book USING(book_id)
+    INNER JOIN author USING(author_id)
+WHERE name_author LIKE 'Достоевский%'
+ORDER BY name_client ASC;
+    
+--9. Вывести жанр (или жанры), в котором было заказано больше всего экземпляров книг, указать это количество . Последний столбец назвать Количество.
+
+/*
+SELECT name_genre, SUM(buy_book.amount) Количество
+FROM 
+    genre
+    INNER JOIN book USING(genre_id)
+    INNER JOIN buy_book USING(book_id)
+GROUP BY name_genre
+ORDER BY Количество DESC
+LIMIT 1;
+*/
+
+SELECT name_genre, SUM(buy_book.amount) Количество
+FROM 
+    genre
+    INNER JOIN book USING(genre_id)
+    INNER JOIN buy_book USING(book_id)
+GROUP BY name_genre
+HAVING SUM(buy_book.amount) = 
+  (SELECT MAX(sum_amount)
+      FROM 
+          (SELECT SUM(buy_book.amount) AS sum_amount 
+            FROM book JOIN buy_book
+                 USING(book_id)
+            GROUP BY genre_id
+            ) query_in);
+
+--10. Сравнить ежемесячную выручку от продажи книг за текущий и предыдущий годы. Для этого вывести год, месяц, сумму выручки в отсортированном сначала по возрастанию месяцев, затем по возрастанию лет виде. Название столбцов: Год, Месяц, Сумма.
+
+DROP TABLE IF EXISTS buy_archive;
+
+CREATE TABLE buy_archive
+(
+    buy_archive_id INT PRIMARY KEY AUTO_INCREMENT,
+    buy_id         INT,
+    client_id      INT,
+    book_id        INT,
+    date_payment   DATE,
+    price          DECIMAL(8, 2),
+    amount         INT
+);
+
+INSERT INTO buy_archive (buy_id, client_id, book_id, date_payment, amount, price)
+VALUES (2, 1, 1, '2019-02-21', 2, 670.60),
+       (2, 1, 3, '2019-02-21', 1, 450.90),
+       (1, 2, 2, '2019-02-10', 2, 520.30),
+       (1, 2, 4, '2019-02-10', 3, 780.90),
+       (1, 2, 3, '2019-02-10', 1, 450.90),
+       (3, 4, 4, '2019-03-05', 4, 780.90),
+       (3, 4, 5, '2019-03-05', 2, 480.90),
+       (4, 1, 6, '2019-03-12', 1, 650.00),
+       (5, 2, 1, '2019-03-18', 2, 670.60),
+       (5, 2, 4, '2019-03-18', 1, 780.90);
+
+SELECT YEAR(date_payment) AS Год, MONTHNAME(date_payment) AS Месяц, SUM(amount * price) AS Сумма
+FROM 
+    buy_archive
+GROUP BY Месяц, Год
+UNION 
+SELECT YEAR(date_step_end) AS Год, MONTHNAME(date_step_end) AS Месяц, SUM(buy_book.amount * price) AS Сумма
+FROM 
+    book
+    INNER JOIN buy_book USING(book_id)
+    INNER JOIN buy USING(buy_id) 
+    INNER JOIN buy_step USING(buy_id)
+    INNER JOIN step USING(step_id)                  
+WHERE  date_step_end IS NOT Null and name_step = "Оплата"
+GROUP BY Месяц, Год
+ORDER BY Месяц, Год;
+
+--11. Для каждой отдельной книги необходимо вывести информацию о количестве проданных экземпляров и их стоимости за текущий и предыдущий год . Вычисляемые столбцы назвать Количество и Сумма. Информацию отсортировать по убыванию стоимости.
+
+SELECT title, SUM(query_in.sum_amount) AS Количество, SUM(query_in.sum_price) AS Сумма
+FROM
+(
+    SELECT title, SUM(buy_archive.amount) AS sum_amount, SUM(buy_archive.amount * buy_archive.price) AS sum_price
+    FROM
+         buy_archive
+         INNER JOIN book USING(book_id)
+    GROUP BY 1
+UNION ALL
+    SELECT title, SUM(buy_book.amount) AS sum_amount, SUM(buy_book.amount * book.price) AS sum_price
+    FROM
+         book
+         INNER JOIN buy_book USING(book_id)
+         INNER JOIN buy_step USING(buy_id)
+    WHERE  date_step_end IS NOT Null AND step_id = 1
+    GROUP BY 1
+) AS query_in 
+GROUP BY 1
+ORDER BY 3 DESC;
+
