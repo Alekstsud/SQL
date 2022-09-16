97319aa341ba79e3fc230a69356cfb41337c28f0
Aleks_Tsud <100237478+Alekstsud@users.noreply.github.com>
2022-09-16 20:53:51 +0200

    refactor: add correction requests

--- DB_testing.sql
+++ DB_testing.sql
@@ -224,6 +224,65 @@ FROM
 GROUP BY 1, 2
 ORDER BY 1, 4 DESC, 2;
 
+--11. В таблицу attempt включить новую попытку для студента Баранова Павла по дисциплине «Основы баз данных». Установить текущую дату в качестве даты выполнения попытки.
 
+INSERT INTO attempt (student_id, subject_id, date_attempt) 
+VALUES 
+    (1, 2, NOW());
 
+SELECT * FROM attempt;
+
+--12. Случайным образом выбрать три вопроса (запрос) по дисциплине, тестирование по которой собирается проходить студент, занесенный в таблицу attempt последним, и добавить их в таблицу testing. id последней попытки получить как максимальное значение id из таблицы attempt.
+
+INSERT INTO testing (attempt_id, question_id) 
+SELECT attempt_id, question_id
+    FROM question 
+    INNER JOIN attempt USING (subject_id)
+    WHERE attempt_id = (SELECT MAX(attempt_id) 
+                        FROM attempt
+                        ) 
+    ORDER BY RAND()
+    LIMIT 3;
+SELECT * FROM testing;
+
+--13. Студент прошел тестирование (то есть все его ответы занесены в таблицу testing), далее необходимо вычислить результат(запрос) и занести его в таблицу attempt для соответствующей попытки.  Результат попытки вычислить как количество правильных ответов, деленное на 3 (количество вопросов в каждой попытке) и умноженное на 100. Результат округлить до целого.
+
+UPDATE attempt
+    SET result = (SELECT ROUND(SUM(is_correct)/3*100, 0)
+        FROM answer INNER JOIN testing ON answer.answer_id = testing.answer_id
+        WHERE attempt_id = 8)
+    WHERE attempt_id = 8;
+
+SELECT * FROM attempt;
+
+--14. Удалить из таблицы attempt все попытки, выполненные раньше 1 мая 2020 года. Также удалить и все соответствующие этим попыткам вопросы из таблицы testing, которая создавалась следующим запросом:
+
+/*CREATE TABLE testing (
+    testing_id INT PRIMARY KEY AUTO_INCREMENT, 
+    attempt_id INT, 
+    question_id INT, 
+    answer_id INT,
+    FOREIGN KEY (attempt_id)  REFERENCES attempt (attempt_id) ON DELETE CASCADE
+);
+*/
+
+--ON DELETE CASCADE - при удалении данных с одной таблицы автоматически удаляются связанные данные с другой. 
+
+DELETE FROM attempt
+WHERE date_attempt < '2020-05-01'; --WHERE DATEDIFF('2020.05.01',date_attempt)<0
+
+SELECT * FROM attempt;
+SELECT * FROM testing;
+
+--15. Добавить в таблицу attempt информацию по всем недостающим попыткам сдачи каждого из студентов по каждому из предметов
+
+INSERT INTO attempt (student_id, subject_id, date_attempt) 
+SELECT student_id, subject_id, NOW()+1
+FROM student
+    CROSS JOIN subject
+    LEFT OUTER JOIN attempt USING (student_id, subject_id)
+WHERE attempt_id IS NULL
+;
+
+SELECT * FROM attempt
 
